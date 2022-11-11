05b476081baae795304c380b6d518641e94393ca
szymon.marciniec <szymon.marciniec@pollub.edu.pl>
2022-11-11 15:31:42 +0100

    init commit

---
+++ Animal.cs
@@ -0,0 +1,11 @@
+﻿namespace AnimalShelter
+{
+    public class Animal
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public int PossibleBirthYear { get; set; }
+
+
+    }
+}
---
+++ Migrations/20221111141015_CreateInitial.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AnimalShelter.Migrations
+{
+    public partial class CreateInitial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Animals",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PossibleBirthYear = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Animals", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Animals");
+        }
+    }
+}
